1e63c1507cd6cd2f1c997eb9df1b9a27b4b7c913
Isaiah-Ferguson <crestice@yahoo.com>
2022-10-17 15:54:38 -0700

    Ready for Peer Review

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿//Isaiah Keith Ferguson
+//10/17/2022
+//Reverse IT
+
+string endingString = "";
+
+while(endingString != "end")
+{
+string input = "";
+string revInput = "";
+int remainder = 0;
+int revNumber = 0;
+bool trueNumber = true;
+int realNumber = 0;
+
+Console.WriteLine("Lets reverse it!");
+Console.WriteLine("Please enter characters");
+
+input = Console.ReadLine();
+trueNumber = Int32.TryParse(input, out realNumber);
+
+if (trueNumber)
+{
+    while (realNumber > 0)
+    {
+        remainder = realNumber % 10;
+        realNumber = realNumber / 10;
+        revNumber = (revNumber * 10) + remainder;
+    }
+Console.WriteLine(input + " | " + revNumber);
+}
+else
+{
+    for (int i = input.Length - 1;i >= 0; i--)
+    {
+        revInput = revInput + input[i];
+    }
+Console.WriteLine(input + " | " + revInput);
+} 
+Console.WriteLine("Please type 'End' To Terminate the program or press 'Enter' to play again");
+endingString = Console.ReadLine().ToLower();
+}
